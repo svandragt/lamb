9382c6b972cc00f6ebdd7448dadf98ce9dcaed25
Sander van Dragt <sander@vandragt.com>
2023-03-17 10:24:58 +0000

    Human time

--- src/views/html.php
+++ src/views/html.php
@@ -29,7 +29,9 @@ function date_created( $bleat ) : string {
 		return '';
 	}
 
-	return sprintf( '<a href="/bleat/%s">%s</a>', $bleat['id'], $bleat['created'] );
+	$created = human_time( strtotime( $bleat['created'] ) );
+
+	return sprintf( '<a href="/bleat/%s" title="%s">%s</a>', $bleat['id'], $bleat['created'], $created );
 }
 
 function parse_tags( $text ) {
@@ -58,6 +60,73 @@ function page_intro() : string {
 	}
 
 	return sprintf( '<p>%s</p>', $data['intro'] );
+}
+
+/**
+ * Thanks to Rose Perrone
+ * @link https://stackoverflow.com/a/11813996
+ */
+function human_time( $timestamp ) {
+	// Get time difference and setup arrays
+	$difference = time() - $timestamp;
+	$periods = [ "second", "minute", "hour", "day", "week", "month", "years" ];
+	$lengths = [ "60", "60", "24", "7", "4.35", "12" ];
+
+	// Past or present
+	if ( $difference >= 0 ) {
+		$ending = "ago";
+	} else {
+		$difference = - $difference;
+		$ending = "to go";
+	}
+
+	// Figure out difference by looping while less than array length
+	// and difference is larger than lengths.
+	$arr_len = count( $lengths );
+	for ( $j = 0; $j < $arr_len && $difference >= $lengths[ $j ]; $j ++ ) {
+		$difference /= $lengths[ $j ];
+	}
+
+	// Round up
+	$difference = round( $difference );
+
+	// Make plural if needed
+	if ( $difference != 1 ) {
+		$periods[ $j ] .= "s";
+	}
+
+	// Default format
+	$text = "$difference $periods[$j] $ending";
+
+	// over 24 hours
+	if ( $j > 2 ) {
+		// future date over a day formate with year
+		if ( $ending === "to go" ) {
+			if ( $j == 3 && $difference == 1 ) {
+				$text = "Tomorrow at " . date( "g:i a", $timestamp );
+			} else {
+				$text = date( "F j, Y \a\\t g:i a", $timestamp );
+			}
+
+			return $text;
+		}
+
+		if ( $j == 3 && $difference == 1 ) // Yesterday
+		{
+			$text = "Yesterday at " . date( "g:i a", $timestamp );
+		} else if ( $j == 3 ) // Less than a week display -- Monday at 5:28pm
+		{
+			$text = date( "l \a\\t g:i a", $timestamp );
+		} else if ( $j < 6 && ! ( $j == 5 && $difference == 12 ) ) // Less than a year display -- June 25 at 5:23am
+		{
+			$text = date( "F j \a\\t g:i a", $timestamp );
+		} else // if over a year or the same month one year ago -- June 30, 2010 at 5:34pm
+		{
+			$text = date( "F j, Y \a\\t g:i a", $timestamp );
+		}
+	}
+
+	return $text;
 }
 
 ?>
