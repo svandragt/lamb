00f44e30d8e638c14f80b2d4276c802330bb7201
Sander van Dragt <sander@vandragt.com>
2023-05-18 10:05:14 +0100

    Sorted register_routes a-z

--- src/index.php
+++ src/index.php
@@ -103,14 +103,14 @@ if ( $action === 'favicon.ico' ) {
 
 Route\register_route( false, __NAMESPACE__ . '\\Response\respond_404' );
 Route\register_route( '404', __NAMESPACE__ . '\\Response\respond_404' );
-Route\register_route( 'status', __NAMESPACE__ . '\\Response\respond_status', $lookup );
-Route\register_route( 'edit', __NAMESPACE__ . '\\Response\respond_edit', $lookup );
 Route\register_route( 'delete', __NAMESPACE__ . '\\Response\redirect_deleted', $lookup );
+Route\register_route( 'edit', __NAMESPACE__ . '\\Response\respond_edit', $lookup );
 Route\register_route( 'feed', __NAMESPACE__ . '\\Response\respond_feed' );
 Route\register_route( 'home', __NAMESPACE__ . '\\Response\respond_home' );
 Route\register_route( 'login', __NAMESPACE__ . '\\Response\redirect_login' );
 Route\register_route( 'logout', __NAMESPACE__ . '\\Response\redirect_logout' );
 Route\register_route( 'search', __NAMESPACE__ . '\\Response\respond_search', $lookup );
+Route\register_route( 'status', __NAMESPACE__ . '\\Response\respond_status', $lookup );
 Route\register_route( 'tag', __NAMESPACE__ . '\\Response\respond_tag', $lookup );
 
 $template = $action;
