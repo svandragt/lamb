e5210e0b2c0d2c62a6905cc8bf54e48d90a60da0
Sander van Dragt <sander@vandragt.com>
2024-01-17 10:16:15 +0000

    More function documentation

--- src/routes.php
+++ src/routes.php
@@ -4,11 +4,27 @@ namespace Svandragt\Lamb\Route;
 
 use Svandragt\Lamb\Response;
 
+/**
+ * Registers a new route.
+ *
+ * @param bool|string $action The action to register. It can be a boolean or a string.
+ * @param string      $callback The callback function to execute when the route is accessed.
+ * @param mixed       ...$args Additional arguments to pass to the callback function.
+ *
+ * @return void
+ */
 function register_route( bool|string $action, string $callback, mixed ...$args ) : void {
 	global $routes;
 	$routes[ $action ] = [ $callback, $args ];
 }
 
+/**
+ * Calls the callback function associated with the specified action and returns the result.
+ *
+ * @param bool|string $action The action to call the callback function for.
+ *
+ * @return array The result of the callback function.
+ */
 function call_route( bool|string $action ) : array {
 	global $routes;
 	[ $callback, $args ] = $routes[ $action ];
@@ -20,7 +36,15 @@ function call_route( bool|string $action ) : array {
 	return $callback( $args );
 }
 
-function is_reserved_route(string $name) : bool {
+/**
+ * Checks if a given route is reserved.
+ *
+ * @param string $name The name of the route to check.
+ *
+ * @return bool True if the route is reserved, false otherwise.
+ */
+function is_reserved_route( string $name ) : bool {
 	global $routes;
-	return isset($routes[$name]);
+
+	return isset( $routes[ $name ] );
 }
--- src/security.php
+++ src/security.php
@@ -1,10 +1,29 @@
 <?php
 
+/**
+ * Checks if a user is logged in. If not, it redirects to the login page
+ *
+ * @since 1.0.0
+ */
+
 namespace Svandragt\Lamb\Security;
 
-use Svandragt\Lamb\Response;
+/**
+ * Class Response
+ *
+ * Represents a response returned by a controller action.
+ */
+
+use    Svandragt\Lamb\Response;
 
 # Security
+/**
+ * Checks if the user is logged in.
+ *
+ * @return void
+ *
+ * If the user is not logged in, a flash message "Please login" is added to the session and the user is redirected to the login page.
+ */
 function require_login() : void {
 	if ( ! isset( $_SESSION[ SESSION_LOGIN ] ) ) {
 		$_SESSION['flash'][] = "Please login";
@@ -12,6 +31,13 @@ function require_login() : void {
 	}
 }
 
+/**
+ * Checks if the CSRF token in the POST request matches the token stored in the session.
+ * If the tokens don't match, sends a 405 Method Not Allowed response and terminates the script.
+ * After successful validation, removes the CSRF token from the session.
+ *
+ * @return void
+ */
 function require_csrf() : void {
 	$token = htmlspecialchars( $_POST[ HIDDEN_CSRF_NAME ] );
 	$csrf = $_SESSION[ HIDDEN_CSRF_NAME ] ?? null;
