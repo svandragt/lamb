defb394daf1a09d55048b068938ec88db3a22fda
Sander van Dragt <sander@vandragt.com>
2024-01-19 12:10:39 +0000

    Manually port over `user-themes` branch

--- src/themes/default/actions/home.php
+++ src/themes/default/actions/home.php
@@ -1,12 +1,4 @@
-<?php if ( isset( $_SESSION[ SESSION_LOGIN ] ) ): ?>
-    <form method="post" action="/" enctype="multipart/form-data">
-        <textarea placeholder="What's happening?" name="contents" required
-        ></textarea>
-        <input type="submit" name="submit" value="<?= SUBMIT_CREATE; ?>">
-        <input type="hidden" name="<?= HIDDEN_CSRF_NAME; ?>" value="<?= csrf_token(); ?>"/>
-    </form>
-<?php endif; ?>
-
+<?= the_entry_form(); ?>
 <?= site_title(); ?>
 <?= page_intro(); ?>
 
--- src/themes/default/html.php
+++ src/themes/default/html.php
@@ -254,6 +254,16 @@ function li_menu_items() {
 	return implode( PHP_EOL, $items );
 }
 
+function the_entry_form() {
+	if ( isset( $_SESSION[ SESSION_LOGIN ] ) ): ?>
+        <form method="post" action="/" enctype="multipart/form-data">
+            <textarea placeholder="What's happening?" name="contents" required></textarea>
+            <input type="submit" name="submit" value="<?= SUBMIT_CREATE; ?>">
+            <input type="hidden" name="<?= HIDDEN_CSRF_NAME; ?>" value="<?= csrf_token(); ?>"/>
+        </form>
+	<?php endif;
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="en-GB">
