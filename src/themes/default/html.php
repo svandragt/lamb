8a90527c310afe462b2b985e6cdab9fbe06c06e3
Sander van Dragt <sander@vandragt.com>
2024-04-19 15:20:06 +0100

    Add label to textarea in entry form

--- src/themes/default/html.php
+++ src/themes/default/html.php
@@ -258,7 +258,9 @@ function li_menu_items() {
 function the_entry_form() {
 	if ( isset( $_SESSION[ SESSION_LOGIN ] ) ): ?>
         <form method="post" action="/" enctype="multipart/form-data">
-            <textarea placeholder="What's happening?" name="contents" required></textarea>
+            <label>
+                <textarea placeholder="What's happening?" name="contents" required></textarea>
+            </label>
             <input type="submit" name="submit" value="<?= SUBMIT_CREATE; ?>">
             <input type="hidden" name="<?= HIDDEN_CSRF_NAME; ?>" value="<?= csrf_token(); ?>"/>
         </form>
