ca33e960b46726d9fda8b774fff40ba597614fdc
Sander van Dragt <sander@vandragt.com>
2024-10-17 17:05:21 +0100

    Input grows on load for editing long posts

--- src/scripts/logged_in/growing-input.js
+++ src/scripts/logged_in/growing-input.js
@@ -1,13 +1,16 @@
 onLoaded(() => {
     let ta = $('textarea')
-    if (!ta) return
-    ta.style.overflow = 'hidden'
-    ta.on('input', growing_input)
-    growing_input({'target': ta})
-});
+    if (!ta) {
+        return
+        ta.on('input', growing_input)
+        growing_input({'target': ta})
+    });
+    }
 
-function growing_input(ev) {
+function growing_input(ev)
+{
+    console.log('recalculating input')
     const target = ev.target
     target.style.height = 'auto'
-    target.style.height = (10 + target.scrollHeight) + 'px'
+    target.style.height = (20 + target.scrollHeight) + 'px'
 }
