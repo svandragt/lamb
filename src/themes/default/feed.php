4b8251c1848a510f57fa55648355d569037d87a2
Sander van Dragt <sander@vandragt.com>
2025-01-11 12:50:53 +0000

    Add 'published' date to Atom feed entries
    This change includes the 'published' date for each feed entry, using the post's creation timestamp. It enhances compliance with the Atom specification and provides more accurate metadata for feed consumers. Indentation inconsistencies in the file were also corrected for better readability.

--- src/themes/default/feed.php
+++ src/themes/default/feed.php
@@ -5,7 +5,7 @@ global $data;
 
 function escape(string $html): string
 {
-    return htmlspecialchars($html, ENT_XML1 | ENT_QUOTES | ENT_SUBSTITUTE);
+	return htmlspecialchars($html, ENT_XML1 | ENT_QUOTES | ENT_SUBSTITUTE);
 }
 
 header('Content-type: application/atom+xml');
@@ -26,13 +26,14 @@ $Author->addChild('name', escape($config['author_name']));
 $Author->addChild('email', escape($config['author_email']));
 
 foreach ($data['posts'] as $bean) {
-    $Entry = $Xml->addChild('entry');
-    $Entry->addChild('id', Lamb\permalink($bean));
-    $Entry->addChild('title', escape($bean->title ?? ''));
-    $Entry->addChild('updated', date(DATE_ATOM, strtotime($bean->updated)));
-    $Content = $Entry->addChild('content', $bean->transformed);
-    $Content->addAttribute('type', 'html');
-    $Link = $Entry->addChild('link');
-    $Link->addAttribute('href', Lamb\permalink($bean));
+	$Entry = $Xml->addChild('entry');
+	$Entry->addChild('id', Lamb\permalink($bean));
+	$Entry->addChild('title', escape($bean->title ?? ''));
+	$Entry->addChild('published', date(DATE_ATOM, strtotime($bean->created)));
+	$Entry->addChild('updated', date(DATE_ATOM, strtotime($bean->updated)));
+	$Content = $Entry->addChild('content', $bean->transformed);
+	$Content->addAttribute('type', 'html');
+	$Link = $Entry->addChild('link');
+	$Link->addAttribute('href', Lamb\permalink($bean));
 }
 echo $Xml->asXML();
