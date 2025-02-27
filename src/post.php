44af4aa9fda02b42a3ea04e02717317d6527254b
Sander van Dragt <sander@vandragt.com>
2025-02-27 15:13:35 +0000

    Added `posts_by_tag` utility to improve tag-based post searches.

--- src/post.php
+++ src/post.php
@@ -22,31 +22,31 @@ use function Lamb\parse_bean;
  */
 function populate_bean(string $text, Item $feed_item = null, string $feed_name = null, OODBBean $bean = null): ?OODBBean
 {
-	$matter = parse_matter($text);
+    $matter = parse_matter($text);
 
-	if (is_null($bean)) {
-		$bean = R::dispense('post');
-	}
-	$bean->body = $text;
-	$bean->slug = $matter['slug'] ?? '';
-	$bean->created = date("Y-m-d H:i:s");
-	$bean->updated = date("Y-m-d H:i:s");
-	if ($feed_item) {
-		$bean->created = $feed_item->get_date("Y-m-d H:i:s");
-		$bean->updated = $feed_item->get_updated_date("Y-m-d H:i:s");
-		if ($feed_name) {
-			if ($bean->slug) {
-				// Prefix with feed name
-				$bean->slug = slugify("$feed_name-" . $bean->slug);
-			}
-			$bean->feeditem_uuid = md5($feed_name . $feed_item->get_id());
-			$bean->feed_name = $feed_name;
-		}
-	}
+    if (is_null($bean)) {
+        $bean = R::dispense('post');
+    }
+    $bean->body = $text;
+    $bean->slug = $matter['slug'] ?? '';
+    $bean->created = date("Y-m-d H:i:s");
+    $bean->updated = date("Y-m-d H:i:s");
+    if ($feed_item) {
+        $bean->created = $feed_item->get_date("Y-m-d H:i:s");
+        $bean->updated = $feed_item->get_updated_date("Y-m-d H:i:s");
+        if ($feed_name) {
+            if ($bean->slug) {
+                // Prefix with feed name
+                $bean->slug = slugify("$feed_name-" . $bean->slug);
+            }
+            $bean->feeditem_uuid = md5($feed_name . $feed_item->get_id());
+            $bean->feed_name = $feed_name;
+        }
+    }
 
-	parse_bean($bean);
+    parse_bean($bean);
 
-	return $bean;
+    return $bean;
 }
 
 /**
@@ -57,29 +57,46 @@ function populate_bean(string $text, Item $feed_item = null, string $feed_name =
  */
 function parse_matter(string $body): array
 {
-	$matter = null;
-	$text = explode('---', $body);
-	try {
-		if (isset($text[1])) {
-			$matter = Yaml::parse($text[1]);
-		}
-	} catch (ParseException) {
-		// Invalid YAML
-		return [];
-	}
+    $matter = null;
+    $text = explode('---', $body);
+    try {
+        if (isset($text[1])) {
+            $matter = Yaml::parse($text[1]);
+        }
+    } catch (ParseException) {
+        // Invalid YAML
+        return [];
+    }
 
-	// There is no matter.
-	if (!is_array($matter)) {
-		return [];
-	}
-	if (isset($matter['title'])) {
-		$matter['slug'] = slugify($matter['title']);
-	}
+    // There is no matter.
+    if (!is_array($matter)) {
+        return [];
+    }
+    if (isset($matter['title'])) {
+        $matter['slug'] = slugify($matter['title']);
+    }
 
-	return $matter;
+    return $matter;
 }
 
 function slugify(string $text): string
 {
-	return strtolower(preg_replace('/\W+/m', "-", $text));
+    return strtolower(preg_replace('/\W+/m', "-", $text));
+}
+
+/**
+ * Retrieves posts that contain the specified tag within their body content.
+ *
+ * @param string $tag The tag to search for within post content.
+ *
+ * @return array An array of posts that match the specified tag.
+ */
+function posts_by_tag(string $tag): array
+{
+    return R::find(
+        'post',
+        'body LIKE ? OR body LIKE ? OR body LIKE ?',
+        ["%#$tag %", "%\n#$tag %", "%#$tag"],
+        'ORDER BY created DESC'
+    );
 }
