70f4e5a2c93f1e0a569954190da4b25fadf842d3
Sander van Dragt <sander@vandragt.com>
2026-03-15 23:58:27 +0000

    Add preconnect support for external origins to improve load times

--- src/themes/default/html.php
+++ src/themes/default/html.php
@@ -5,6 +5,7 @@ use function Lamb\Theme\li_menu_items;
 use function Lamb\Theme\part;
 use function Lamb\Theme\site_or_page_title;
 use function Lamb\Theme\the_opengraph;
+use function Lamb\Theme\the_preconnect;
 use function Lamb\Theme\the_scripts;
 use function Lamb\Theme\the_styles;
 
@@ -21,6 +22,8 @@ global $template;
     <title><?= escape(site_or_page_title('text')) ?></title>
     <link rel="alternate" type="application/atom+xml" href="<?= ROOT_URL . '/feed' ?>"
           title="<?= escape($config['site_title']) ?>">
+    <?php
+    the_preconnect(); ?>
     <?php
     the_styles(); ?>
     <?php
