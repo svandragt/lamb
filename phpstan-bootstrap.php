78ed0e8df2768e200050b530556d86095d292ff4
Sander van Dragt <sander@vandragt.com>
2026-03-19 12:59:16 +0000

    Fix inconsistent indentation in phpstan-bootstrap.php for improved readability and maintainability

--- phpstan-bootstrap.php
+++ phpstan-bootstrap.php
@@ -2,35 +2,35 @@
 
 namespace JetBrains\PhpStorm {
 
-use Attribute;
+    use Attribute;
 
-if (!class_exists(NoReturn::class)) {
-    #[Attribute(Attribute::TARGET_FUNCTION | Attribute::TARGET_METHOD)]
-    final class NoReturn
-    {
+    if (!class_exists(NoReturn::class)) {
+        #[Attribute(Attribute::TARGET_FUNCTION | Attribute::TARGET_METHOD)]
+        final class NoReturn
+        {
+        }
     }
-}
 }
 
 namespace {
 
-if (!defined('ROOT_DIR')) {
-    define('ROOT_DIR', __DIR__ . '/src');
-}
+    if (!defined('ROOT_DIR')) {
+        define('ROOT_DIR', __DIR__ . '/src');
+    }
 
-if (!defined('ROOT_URL')) {
-    define('ROOT_URL', 'http://localhost');
-}
+    if (!defined('ROOT_URL')) {
+        define('ROOT_URL', 'http://localhost');
+    }
 
-if (!defined('THEME')) {
-    define('THEME', 'default');
-}
+    if (!defined('THEME')) {
+        define('THEME', 'default');
+    }
 
-if (!defined('THEME_DIR')) {
-    define('THEME_DIR', ROOT_DIR . '/themes/' . THEME . '/');
-}
+    if (!defined('THEME_DIR')) {
+        define('THEME_DIR', ROOT_DIR . '/themes/' . THEME . '/');
+    }
 
-if (!defined('THEME_URL')) {
-    define('THEME_URL', 'themes/' . THEME . '/');
-}
+    if (!defined('THEME_URL')) {
+        define('THEME_URL', 'themes/' . THEME . '/');
+    }
 }
