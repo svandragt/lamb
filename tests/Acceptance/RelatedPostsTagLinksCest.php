4ea8975ab05da37618b42441836b89ea66c96180
Sander van Dragt <sander@vandragt.com>
2026-03-28 12:58:57 +0000

    Fix related tags tests

--- tests/Acceptance/RelatedPostsTagLinksCest.php
+++ tests/Acceptance/RelatedPostsTagLinksCest.php
@@ -6,9 +6,32 @@ use Tests\Support\AcceptanceTester;
 
 class RelatedPostsTagLinksCest
 {
-    public function relatedPostsContainLinkedTags(AcceptanceTester $I)
+    private function login(AcceptanceTester $I): void
     {
-        $I->amOnPage('/status/111');
+        $I->amOnPage('/login');
+        $I->fillField('password', $_ENV['LAMB_TEST_PASSWORD']);
+        $I->click('Log in');
+    }
+
+    private function createPost(AcceptanceTester $I, string $contents): void
+    {
+        $I->amOnPage('/');
+        $I->fillField('contents', $contents);
+        $I->click('Create post');
+    }
+
+    public function relatedPostsContainLinkedTags(AcceptanceTester $I): void
+    {
+        $tag = 'relatedtagtest' . uniqid();
+
+        $this->login($I);
+        $this->createPost($I, "First post about #$tag");
+        $this->createPost($I, "Second post about #$tag");
+
+        // Grab the URL of the most recent post (top of homepage)
+        $postUrl = $I->grabAttributeFrom('article small a[href^="/status/"]', 'href');
+        $I->amOnPage($postUrl);
+
         $I->seeElement('.related-posts a[href^="/tag/"]');
     }
 }
